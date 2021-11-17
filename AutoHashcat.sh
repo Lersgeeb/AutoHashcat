6b531ab0d1e781f0a56619ba57a55095ebbe46ba
jrgdiaz <jrgdiaz809@gmail.com>
2021-11-17 00:03:16 -0400

    Update AutoHashcat.sh

--- AutoHashcat.sh
+++ AutoHashcat.sh
@@ -26,25 +26,22 @@ MASK_LIST="corp_8.hcmask corp_9.hcmask"
         		$HASHCAT_BIN -a 0 -m $HASH_TYPE -w4 -r $RULES_DIRECTORY$b $HASHES_FILE $WORDLIST_DIRECTORY$a -O --potfile-path $POTFILE.pot
     		done
 	done
-: '
-
 
 	for a in $MASK_LIST; do
-    		echo $HASHCAT_BIN -a 3 -m $HASH_TYPE -w4 $HASHES_FILE $MASKS_DIRECTORY$a -O --potfile-path $POTFILE.pot
+    		 $HASHCAT_BIN -a 3 -m $HASH_TYPE -w4 $HASHES_FILE $MASKS_DIRECTORY$a -O --potfile-path $POTFILE.pot
 	done
 
 	for a in $WORDLIST; do
     		for b in $MASK_LIST; do
-        		echo $HASHCAT_BIN -a 6 -m $HASH_TYPE -w4 $HASHES_FILE $WORDLIST_DIRECTORY$a $MASKS_DIRECTORY$b -O --potfile-path $POTFILE.pot
+        		$HASHCAT_BIN -a 6 -m $HASH_TYPE -w4 $HASHES_FILE $WORDLIST_DIRECTORY$a $MASKS_DIRECTORY$b -O --potfile-path $POTFILE.pot
     		done
 	done
 
 	for a in $WORDLIST; do
     		for b in $MASK_LIST; do
-        		echo $HASHCAT_BIN -a 7 -m $HASH_TYPE -w4 $HASHES_FILE $MASKS_DIRECTORY$b $WORDLIST_DIRECTORY$a -O --potfile-path $POTFILE.pot
+        		$HASHCAT_BIN -a 7 -m $HASH_TYPE -w4 $HASHES_FILE $MASKS_DIRECTORY$b $WORDLIST_DIRECTORY$a -O --potfile-path $POTFILE.pot
     		done
 	done
 
-	echo $HASHCAT_BIN -i -a 3 -m $HASH_TYPE -w4 $HASHES_FILE ?a?a?a?a?a?a?a -O --potfile-path $POTFILE.pot
-'
+	 $HASHCAT_BIN -i -a 3 -m $HASH_TYPE -w4 $HASHES_FILE ?a?a?a?a?a?a?a -O --potfile-path $POTFILE.pot
 fi
